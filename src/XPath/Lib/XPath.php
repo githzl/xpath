cf776836c67eb17ae322377292901d6e1ccbbe8f
16955276@qq.com <123123>
2018-07-31 22:17:28 +0800

    update

---
+++ src/XPath/Lib/XPath.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace XPath\Lib\XPath;
+
+
+class XPath{
+
+    public $text; // 整个html文档的文本
+
+    public function __construct($text = null)
+    {
+        $this->text = $text;
+    }
+
+    public static function test()
+    {
+        return 'this is test';
+    }
+}
